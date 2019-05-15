5628878ff6b07823d1d68e915f14613ef40cdbb4
Helisky <skhely998@gmail.com>
2019-05-15 14:29:05 -0600

    Identacion de insert.php

--- insert.php
+++ insert.php
@@ -6,27 +6,28 @@
     </head>
     <body>
         <?php
-        $servername = "localhost";
-        $username = "root";
-        $password = "hely12345";
-        $dbname = "products";
+ $servername = "localhost";
+ $username   = "root";
+ $password   = "hely12345";
+ $dbname     = "products";
 
-        $pro = $_GET["product"];
-        $pri = $_GET["price"];
-        $act = isset($_GET['che']) ? 1 : 0;
+ $pro = $_GET["product"];
+ $pri = $_GET["price"];
+ $act = isset($_GET['che']) ? 1 : 0;
 
-        try {
-           $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-           $sql = "INSERT INTO product VALUES (null, '$pro', '$pri', '$act')";
-           $conn->exec($sql);
-           echo "New record created successfully";
-           }
-        catch(PDOException $e)
-           {
-           echo $sql . "<br>" . $e->getMessage();
-           }
-        $conn = null;
-        ?>
+ try {
+     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username,
+      $password);
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $sql = "INSERT INTO product VALUES (null, '$pro', '$pri', '$act')";
+     $conn->exec($sql);
+     echo "New record created successfully";
+ }
+ catch (PDOException $e) {
+     echo $sql . "<br>" . $e->getMessage();
+ }
+ $conn = null;
+ header("Location: ind.html");
+ ?>
     </body>
 </html>
